8d2d09e7b6444a4f26ffdd95de9c19764b50d2ff
克鲁鲁 <3418001848@qq.com>
2026-06-22 11:05:40 +0800

    提交2

---
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,21 @@
+package com.atguigu.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+//@ConfigurationPropertiesScan("com.atguigu.lease.common.minio")
+public class MinioConfiguration {
+    @Bean
+    public MinioClient minioClient(MinioProperties minioProperties) {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+}
