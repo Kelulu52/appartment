3cc389da37ab33b06dfa4cbd8188aa79cc7da92a
克鲁鲁 <3418001848@qq.com>
2026-06-26 10:28:02 +0800

    修改和更新公寓管理接口实现

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
@@ -1,10 +1,20 @@
 package com.atguigu.lease.web.admin.service.impl;
 
-import com.atguigu.lease.model.entity.ApartmentInfo;
+import com.atguigu.lease.model.entity.*;
+import com.atguigu.lease.model.enums.ItemType;
 import com.atguigu.lease.web.admin.mapper.ApartmentInfoMapper;
-import com.atguigu.lease.web.admin.service.ApartmentInfoService;
+import com.atguigu.lease.web.admin.service.*;
+import com.atguigu.lease.web.admin.vo.apartment.ApartmentSubmitVo;
+import com.atguigu.lease.web.admin.vo.graph.GraphVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author liubo
@@ -15,6 +25,79 @@ import org.springframework.stereotype.Service;
 public class ApartmentInfoServiceImpl extends ServiceImpl<ApartmentInfoMapper, ApartmentInfo>
         implements ApartmentInfoService {
 
+    @Autowired
+    private GraphInfoService graphInfoService;
+    @Autowired
+    private ApartmentFacilityService apartmentFacilityService;
+    @Autowired
+    private ApartmentLabelService apartmentLabelService;
+    @Autowired
+    private ApartmentFeeValueService apartmentFeeValueService;
+    @Override
+    public void saveOrUpdateApartment(ApartmentSubmitVo apartmentSubmitVo) {
+        boolean isupdate=apartmentSubmitVo.getId()!=null;
+        super.saveOrUpdate(apartmentSubmitVo);
+        if(isupdate){
+            LambdaQueryWrapper<GraphInfo> GraphqueryWrapper=new LambdaQueryWrapper<>();
+            GraphqueryWrapper.eq(GraphInfo::getItemType, ItemType.APARTMENT);
+            GraphqueryWrapper.eq(GraphInfo::getId, apartmentSubmitVo.getId());
+            graphInfoService.remove(GraphqueryWrapper);
+
+            LambdaQueryWrapper<ApartmentFacility> apartmentFacilityQueryWrapper = new LambdaQueryWrapper<>();
+            apartmentFacilityQueryWrapper.eq(ApartmentFacility::getId, apartmentSubmitVo.getId());
+            apartmentFacilityService.remove(apartmentFacilityQueryWrapper);
+
+            LambdaQueryWrapper<ApartmentLabel> apartmentLabelLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            apartmentLabelLambdaQueryWrapper.eq(ApartmentLabel::getId, apartmentSubmitVo.getId());
+            apartmentLabelService.remove(apartmentLabelLambdaQueryWrapper);
+
+            LambdaQueryWrapper<ApartmentFeeValue> apartmentFeeValueLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            apartmentFeeValueLambdaQueryWrapper.eq(ApartmentFeeValue::getId, apartmentSubmitVo.getId());
+            apartmentFeeValueService.remove(apartmentFeeValueLambdaQueryWrapper);
+        }
+        List<GraphVo> graphVoList = apartmentSubmitVo.getGraphVoList();
+        if(!CollectionUtils.isEmpty(graphVoList)){
+            ArrayList<GraphInfo> graphInfoList = new ArrayList<>();
+            for (GraphVo graphVo : graphVoList) {
+                GraphInfo graphInfo = new GraphInfo();
+                graphInfo.setItemType(ItemType.APARTMENT);
+                graphInfo.setItemId(apartmentSubmitVo.getId());
+                graphInfo.setUrl(graphVo.getUrl());
+                graphInfo.setName(graphVo.getName());
+                graphInfoList.add(graphInfo);
+            }
+            graphInfoService.saveBatch(graphInfoList);
+
+        }
+        List<Long> facilityInfoIdList = apartmentSubmitVo.getFacilityInfoIds();
+        if(!CollectionUtils.isEmpty(facilityInfoIdList)){
+            ArrayList<ApartmentFacility> facilityList = new ArrayList<>();
+            for (Long facility : facilityInfoIdList) {
+                ApartmentFacility facilitys=ApartmentFacility.builder().apartmentId(apartmentSubmitVo.getId()).facilityId(facility).build();
+                facilityList.add(facilitys);
+            }
+            apartmentFacilityService.saveBatch(facilityList);
+        }
+        List<Long> labelIds = apartmentSubmitVo.getLabelIds();
+        if(!CollectionUtils.isEmpty(labelIds)){
+            ArrayList<ApartmentLabel> labelList = new ArrayList<>();
+            for (Long label : labelIds) {
+                ApartmentLabel apartmentLabel=ApartmentLabel.builder().apartmentId(apartmentSubmitVo.getId()).labelId(label).build();
+                labelList.add(apartmentLabel);
+            }
+            apartmentLabelService.saveBatch(labelList);
+        }
+        List<Long> feeValueIds = apartmentSubmitVo.getFeeValueIds();
+        if(!CollectionUtils.isEmpty(feeValueIds)){
+            ArrayList<ApartmentFeeValue> feeValueList = new ArrayList<>();
+            for (Long feeValue : feeValueIds) {
+                ApartmentFeeValue apartmentFeeValue=ApartmentFeeValue.builder().apartmentId(apartmentSubmitVo.getId()).feeValueId(feeValue).build();
+                feeValueList.add(apartmentFeeValue);
+            }
+            apartmentFeeValueService.saveBatch(feeValueList);
+        }
+
+    }
 }
 
 
