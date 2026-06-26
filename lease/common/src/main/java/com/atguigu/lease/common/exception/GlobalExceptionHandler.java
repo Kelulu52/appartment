dba83b0d908b9e3284d56ccb0fb3c4b6c648ae13
克鲁鲁 <3418001848@qq.com>
2026-06-26 15:45:28 +0800

    查找公寓管理接口实现

--- lease/common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
+++ lease/common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.atguigu.lease.common.exception;
 
 import com.atguigu.lease.common.result.Result;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Hidden
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
