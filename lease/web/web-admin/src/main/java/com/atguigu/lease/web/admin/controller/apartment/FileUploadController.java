35aeef3ebc30af16df57854c04d0288e3438298d
克鲁鲁 <3418001848@qq.com>
2026-07-04 10:09:43 +0800

    获取用户登录信息实现

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FileUploadController.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FileUploadController.java
@@ -7,10 +7,8 @@ import io.minio.errors.MinioException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -24,10 +22,9 @@ public class FileUploadController {
     @Autowired
     private FileService fileService;
     @Operation(summary = "上传文件")
-    @PostMapping("upload")
-    public Result<String> upload(@RequestParam MultipartFile file) throws MinioException, IOException {
-         String url = fileService.upload(file);
-         return Result.ok(url);
+    @PostMapping(value = "upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Result<String> upload(@RequestPart("file") MultipartFile file) throws MinioException, IOException {
+        String url = fileService.upload(file);
+        return Result.ok(url);
     }
-
 }
